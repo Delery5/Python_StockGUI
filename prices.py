c44ebcaa5230e6d182aae41da93019ee62b8c17a
Delery Harrison <harrisondelery@yahoo.com>
2023-08-01 11:44:09 -0500

    first commit

---
+++ prices.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 19:40:55 2023
+
+@author: deleryharrison
+"""
+
+sum = 0
+count = 0
+full_name = input("What is your full name: ")
+#You will write the rest of the code
+print("Hello", full_name)
